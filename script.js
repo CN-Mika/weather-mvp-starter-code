5238c6d96eec06e37a51edbf191fa7260576d14b
Glitch (weather-mvp-starter-code) <none>
2022-05-10 16:50:17 +0000

    ⚡🌗 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,17 @@
+//weather api url
+//http://api.openweathermap.org/data/2.5/weather?q=Cedar%20Rapids&APPID=3bb00f30e525b91a1deb9cbd20254379
+
+//declare global variables
+
+//include a click handler for search bar
+
+//save user input for city
+//save user input for state
+
+//update weather api
+
+//add fetch request using weather api
+
+//then with function that returns an api response in json
+
+//then with function that displays data to page
